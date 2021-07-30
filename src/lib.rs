2afdd60390d5592bdf1263b42a6ae67e51e1f625
Jeremy Knope <jeremy@astropad.com>
2021-07-30 11:00:50 -0400

    fix: get macOS/apple building again for register

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 
 #![forbid(missing_docs)]
 
-use thiserror::Error;
-
 // pub mod browser;
 mod ffi;
 #[cfg(feature = "non-blocking")]
@@ -18,19 +16,5 @@ pub use crate::register::DNSServiceBuilder;
 #[macro_use]
 extern crate log;
 
-/// Common error for DNS-SD service
-#[derive(Debug, Error, Copy, Clone, PartialEq, Eq)]
-pub enum DNSServiceError {
-    /// Invalid input string
-    #[error("Invalid string argument, must be C string compatible")]
-    InvalidString,
-    /// Unexpected invalid strings from C API
-    #[error("DNSSD API returned invalid UTF-8 string")]
-    InternalInvalidString,
-    /// Error from DNSSD service
-    #[error("DNSSD Error: {0}")]
-    ServiceError(i32),
-}
-
-/// Result type for dns-sd fallible returns
-pub type Result<T, E = RegistrationError> = std::result::Result<T, E>;
+// /// Result type for dns-sd fallible returns
+// pub type Result<T, E = RegistrationError> = std::result::Result<T, E>;
