5c8c9d80198d5e6d4dd44f1cd98e0192ab2b2826
Jeremy Knope <jeremy@astropad.com>
2021-08-06 14:31:34 -0400

    chore: cleanup of commented code

--- examples/register.rs
+++ examples/register.rs
@@ -7,8 +7,6 @@ use std::time::Duration;
 fn main() {
     env_logger::from_env(Env::default().default_filter_or("trace")).init();
     println!("Registering service...");
-    // let mut txt: HashMap<String, String> = HashMap::new();
-    // let _ = txt.insert("status".into(), "open".into());
     let service = DNSServiceBuilder::new("_http._tcp", 8080)
         .with_key_value("status".into(), "open".into())
         .register();
@@ -27,17 +25,4 @@ fn main() {
     }
     log::info!("Drop should have happened");
     sleep(Duration::from_secs(5));
-    // let mut service = DNSServiceBuilder::new("_http._tcp")
-    //     .with_port(2048)
-    //     .with_name("MyRustService")
-    //     .with_txt_record(txt)
-    //     .build()
-    //     .unwrap();
-    // let _result = service.register(|reply| match reply {
-    //     Ok(reply) => println!("Successful reply: {:?}", reply),
-    //     Err(e) => println!("Error registering: {:?}", e),
-    // });
-    // loop {
-    //     service.process_result();
-    // }
 }
